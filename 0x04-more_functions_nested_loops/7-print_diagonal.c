d6d54078d27a02b8c25912c47e0bd20f24d6444a
muhammeddahman <mohamedyasserdahman@gmail.com>
2023-07-20 16:09:46 -0700

    ww

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -18,7 +18,10 @@ void print_diagonal(int n)
 		for (postn = 1; postn <= n; postn++)
 		{
 			for (space = 1; space <= postn; space++)
+			{
+			
 				_putchar(' ');
+			}
 			_putchar(92); /* equal to '/' char*/
 			_putchar('\n');
 		}
